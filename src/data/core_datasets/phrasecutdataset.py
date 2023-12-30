962f68b177461630ba9fec0f6aeee6653ccb42d5
Rabin Adhikari <rabin.adk1@gmail.com>
2023-12-30 20:47:08 +0545

    Add prompt_method and neg_prob to the phrasecut dataset

--- src/data/core_datasets/phrasecutdataset.py
+++ src/data/core_datasets/phrasecutdataset.py
@@ -1,10 +1,12 @@
 # pyright: reportGeneralTypeIssues=false
-import json
+import random
+from collections import defaultdict
 from pathlib import Path
-from typing import Callable, Iterable, Literal, Mapping, Optional, Tuple, Union
+from typing import Callable, Iterable, List, Literal, Mapping, Optional, Tuple, Union
 
 import cv2
 import cv2.typing
+import ijson
 import numpy as np
 from matplotlib import pyplot as plt
 from torch.utils.data import Dataset
@@ -13,6 +15,8 @@ from transformers import AutoTokenizer
 StrPath = Union[str, Path]
 
 PolygonType = Iterable[Iterable[Iterable[Tuple[int, int]]]]
+TaskType = Mapping[str, Union[str, PolygonType]]
+PromptMethodType = Literal["fixed", "shuffle", "shuffle+"]
 
 
 class PhraseCutDataset(Dataset):
@@ -23,16 +27,19 @@ class PhraseCutDataset(Dataset):
         tokenizer_pretrained_path: StrPath,
         image_dir: StrPath = "images",
         transforms: Optional[Callable] = None,
-        return_tensors: Literal["tf", "pt", "np"] = "np",
+        return_tensors: Optional[Literal["tf", "pt", "np"]] = None,
+        prompt_method: PromptMethodType = "fixed",
+        neg_prob: float = 0.0,
+        neg_sample_tries: int = 1000,
     ) -> None:
         super().__init__()
 
         data_root = Path(data_root)
 
-        with (data_root / task_json_path).open() as f:
-            self.tasks: Tuple[Mapping[str, Union[str, PolygonType]], ...] = tuple(
-                json.load(f),
-            )
+        self.tasks = self.get_tasks(data_root / task_json_path)
+
+        self.phrase2image_ids = self.get_phrase2image_ids()
+        self.unique_phrases = tuple(self.phrase2image_ids)
 
         self.image_path = data_root / image_dir
 
@@ -41,30 +48,89 @@ class PhraseCutDataset(Dataset):
 
         self.transforms = transforms
 
+        self.neg_prob = neg_prob
+        self.neg_sample_tries = neg_sample_tries
+
+        self.prompt_format_choices = self.get_prompt_list(prompt_method)
+
+    def get_phrase2image_ids(self):
+        # Map phrase to a list of images
+        phrase2image_ids: Mapping[str, List[str]] = defaultdict(list)
+
+        for task in self.tasks:
+            phrase = task["phrase"]
+            image_id = task["image_id"]
+
+            phrase2image_ids[phrase].append(image_id)
+
+        return {k: set(v) for k, v in phrase2image_ids.items()}
+
+    @staticmethod
+    def get_tasks(json_path: StrPath):
+        task_list: List[TaskType] = []
+        required_keys = ("phrase", "Polygons", "image_id")
+
+        # Load json iteratively
+        with open(json_path, "rb") as f:
+            for item in ijson.items(f, "item", use_float=True):
+                task: TaskType = {k: v for k, v in item.items() if k in required_keys}
+                task_list.append(task)
+
+        return tuple(task_list)
+
+    @staticmethod
+    def get_prompt_list(prompt_method: PromptMethodType):
+        prompt_format_list = ["a photo of a {}."]
+
+        # For shuffle and shuffle+
+        if prompt_method != "fixed":
+            prompt_format_list.extend(
+                (
+                    "a photograph of a {}.",
+                    "an image of a {}.",
+                    "{}.",
+                )
+            )
+
+        if prompt_method == "shuffle+":
+            prompt_format_list.extend(
+                (
+                    "a cropped photo of a {}.",
+                    "a good photo of a {}.",
+                    "a photo of one {}.",
+                    "a bad photo of a {}.",
+                    "a photo of the {}.",
+                )
+            )
+
+        return tuple(prompt_format_list)
+
     def __len__(self) -> int:
         return len(self.tasks)
 
     def __getitem__(self, idx: int):
         task = self.tasks[idx]
 
-        image = self.load_image(
-            self.image_path / f"{task['image_id']}.jpg",
-            cv2.IMREAD_COLOR,
-        )
+        image_id = task["image_id"]
+
+        image = self.load_image(self.image_path / f"{image_id}.jpg", cv2.IMREAD_COLOR)
+
         # Convert BGR to RGB
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        text_output = self.tokenizer(
-            task["phrase"],
-            truncation=True,
-            padding="max_length",
-            return_tensors=self.return_tensors,
+        phrase = task["phrase"]
+        new_phrase_or_none = self.get_neg_phrase(
+            curr_phrase=phrase, curr_image_id=image_id
         )
 
-        # Remove the first dimension
-        text_output = {k: v[0] for k, v in text_output.items()}
-
-        mask = self.polygon_to_mat(image.shape[:-1], task["Polygons"])
+        mask_shape = image.shape[:-1]
+        if new_phrase_or_none is not None:
+            # Replace phrase and make mask zeros
+            phrase = new_phrase_or_none
+            mask = np.zeros((*mask_shape, 1), np.float32)
+        else:
+            # Get polygon from mask if new_phrase is not extracted
+            mask = self.polygon_to_mat(mask_shape, task["Polygons"])
 
         # Add the final channel layer to mask
         mask = mask[..., None]
@@ -74,7 +140,50 @@ class PhraseCutDataset(Dataset):
             image = transformed["image"]
             mask = transformed["mask"]
 
-        return {"image": image, "mask": mask, **text_output}
+        text_inputs = self.get_text_output(phrase)
+
+        return {"image": image, "mask": mask, **text_inputs}
+
+    def get_text_output(self, phrase: str):
+        # Get a format randomly if prompt_format_list has more than one entries
+        prompt_format = (
+            self.prompt_format_choices[0]
+            if len(self.prompt_format_choices)
+            else random.choice(self.prompt_format_choices)
+        )
+
+        prompt = prompt_format.format(phrase)
+
+        # Tokenize text
+        text_inputs = self.tokenizer(
+            prompt,
+            truncation=True,
+            padding="max_length",
+            return_tensors=self.return_tensors,
+        )
+
+        # Remove the first (batch) dimension
+        return {k: v[0] for k, v in text_inputs.items()}
+
+    def get_neg_phrase(self, curr_phrase: str, curr_image_id: str) -> Optional[str]:
+        # Use zero mask if neg_prob is greater than 1
+        # Do not use mask if the neg_prob is less than 0
+        if self.neg_prob >= 1 or (
+            self.neg_prob > 0 and random.random() < self.neg_prob
+        ):
+            for _ in range(self.neg_sample_tries):
+                new_phrase = random.choice(self.unique_phrases)
+
+                # Can not sample the same phrase
+                if new_phrase == curr_phrase:
+                    continue
+
+                new_image_ids = self.phrase2image_ids[new_phrase]
+
+                # Do not get the phrase if it is present in the dataset for the same image
+                if curr_image_id not in new_image_ids:
+                    return new_phrase
+        return None
 
     @staticmethod
     def load_image(path: StrPath, flags: int = cv2.IMREAD_UNCHANGED):
