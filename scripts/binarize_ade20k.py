d16ce7af25065a666f5bb23344684067fd87e0a7
Rabin Adhikari <rabin.adk1@gmail.com>
2024-05-28 12:39:22 +0545

    Feat(scripts): Added scripts to binarize ade20k and cityscapes to the repo

--- scripts/binarize_ade20k.py
+++ scripts/binarize_ade20k.py
@@ -1,4 +1,6 @@
+import concurrent.futures
 import shutil
+import typing
 from pathlib import Path
 from typing import Literal
 
@@ -161,8 +163,45 @@ ADE_classes = (
 )
 
 
+non_background_classes = ADE_classes[1:]
+
+SplitType = Literal["training", "validation"]
+
+
+def process_single_file(
+    ADE20K_images_path: Path,
+    cls_annotations_dump_path: Path,
+    idx: float,
+    images_dump_path: Path,
+    mask_file: Path,
+):
+    with Image.open(mask_file) as img:
+        mask = np.asarray(img)
+
+    if idx not in mask:
+        return False
+
+    mask = (mask == idx).astype(np.uint8) * 255
+    mask = Image.fromarray(mask, mode="L")
+
+    mask_new_path = cls_annotations_dump_path / mask_file.name
+    mask.save(mask_new_path, optimize=True)
+
+    image_basename = f"{mask_file.stem}.jpg"
+
+    image_old_path = ADE20K_images_path / image_basename
+
+    image_new_path = images_dump_path / image_basename
+
+    # If the image file already exists skip the copy part below
+    if not image_new_path.exists():
+        shutil.copy(image_old_path, image_new_path)
+
+    return True
+
+
 def prepare_common_split(
-    ADE20K_path: Path, dump_path: Path, split: Literal["training", "validation"]
+    ADE20K_path: Path, dump_path: Path, max_workers: int | None, split: SplitType
 ):
     images_dump_path = dump_path / "images" / split
     images_dump_path.mkdir(parents=True, exist_ok=True)
@@ -170,46 +209,61 @@ def prepare_common_split(
     annotations_dump_path = dump_path / "annotations" / split
     annotations_dump_path.mkdir(parents=True, exist_ok=True)
 
-    start_idx = 1
-    # Skip background class
-    useful_classes = ADE_classes[start_idx:]
-    with tqdm(useful_classes, initial=start_idx) as classes:
-        for idx, cls in enumerate(classes, start_idx):
-            mask_files = tuple(ADE20K_path.glob(f"annotations/{split}/*.png"))
+    ADE20K_images_path = ADE20K_path / "images" / split
+    if not ADE20K_images_path.is_dir():
+        raise FileNotFoundError(f"{ADE20K_images_path} not found")
 
+    ADE20K_annotation_path = ADE20K_path / "annotations" / split
+    mask_files = tuple(ADE20K_annotation_path.glob("*.png"))
+
+    if not mask_files:
+        raise FileNotFoundError(f"No masks files found in: {ADE20K_annotation_path}")
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+        future_to_desc: dict[
+            concurrent.futures.Future[bool], tuple[str, Path, str]
+        ] = {}
+
+        for idx, cls in enumerate(non_background_classes, 1):
             cls_annotations_dump_path = annotations_dump_path / cls
             cls_annotations_dump_path.mkdir(exist_ok=True)
 
             for mask_idx, mask_file in enumerate(mask_files, 1):
-                classes.set_description(
+                f = executor.submit(
+                    process_single_file,
+                    ADE20K_images_path,
+                    cls_annotations_dump_path,
+                    idx,
+                    images_dump_path,
+                    mask_file,
+                )
+
+                desc = (
                     f"For {split} -- class: '{cls}', "
                     f"scanned {mask_idx}/{len(mask_files)}"
                 )
+                future_to_desc[f] = (cls, mask_file, desc)
 
-                with Image.open(mask_file) as img:
-                    mask = np.asarray(img)
-
-                if idx not in mask:
-                    continue
-
-                mask = (mask == idx).astype(np.uint8) * 255
-                mask = Image.fromarray(mask, mode="L")
-
-                mask_new_path = cls_annotations_dump_path / mask_file.name
-                mask.save(mask_new_path, optimize=True)
-
-                image_basename = f"{mask_file.stem}.jpg"
-
-                image_old_path = ADE20K_path / "images" / split / image_basename
-
-                image_new_path = images_dump_path / image_basename
-
-                # If the image file already exists skip the copy part below
-                if not image_new_path.exists():
-                    shutil.copy(image_old_path, image_new_path)
+        with tqdm(
+            concurrent.futures.as_completed(future_to_desc),
+            total=len(future_to_desc),
+        ) as pbar:
+            for future in pbar:
+                cls, mask_file, desc = future_to_desc[future]
+                try:
+                    ok = future.result()
+                except Exception as exc:
+                    print(
+                        f"Class name: {cls}, Mask file: {mask_file} generated an exception: {exc}"
+                    )
+                else:
+                    if ok:
+                        pbar.set_description(desc)
 
 
-def main(ADE20K_path: Path, dump_path: Path):
+def main(
+    ADE20K_path: Path, dump_path: Path, split: SplitType | None, max_workers: int | None
+):
     """Prepare the ADE20K dataset compatible for few shot training.
         If shot (K) is 16, a folder sibling to ADE20K_path  is created with name '16_shot', having the structure:
             <dump_path>
@@ -232,13 +286,14 @@ def main(ADE20K_path: Path, dump_path: Path):
         ADE20K_path: The path of the ADEChallengeData2016 dataset for
     """
 
-    common_args = (ADE20K_path, dump_path)
+    common_args = (ADE20K_path, dump_path, max_workers)
 
-    # Prepare train dataset
-    prepare_common_split(*common_args, split="training")
-
-    # Prepare validation dataset
-    prepare_common_split(*common_args, split="validation")
+    if split is None:
+        for s in typing.get_args(SplitType):
+            prepare_common_split(*common_args, split=s)
+            print(f"{s} split done!\n")
+    else:
+        prepare_common_split(*common_args, split=split)
 
 
 if __name__ == "__main__":
@@ -264,6 +319,19 @@ if __name__ == "__main__":
         type=Path,
         help="The path to save new dataset",
         required=True,
+    )
+    parser.add_argument(
+        "--split",
+        type=str,
+        help="The split of dataset to be processed",
+        default=None,
+        choices=typing.get_args(SplitType),
+    )
+    parser.add_argument(
+        "--max-workers",
+        type=int,
+        default=None,
+        help="The number of workers for multiprocessing",
     )
 
     args = parser.parse_args()
--- scripts/binarize_cityscapes.py
+++ scripts/binarize_cityscapes.py
@@ -1,5 +1,6 @@
+import concurrent.futures
 import shutil
-from collections.abc import Sequence
+import typing
 from pathlib import Path
 from typing import Literal, NamedTuple
 
@@ -50,11 +51,95 @@ class Label(NamedTuple):
     color: tuple[int, int, int]
 
 
+labels = (
+    #       name                     id    trainId   category            catId     hasInstances   ignoreInEval   color
+    Label("unlabeled", 0, 255, "void", 0, False, True, (0, 0, 0)),
+    Label("ego vehicle", 1, 255, "void", 0, False, True, (0, 0, 0)),
+    Label("rectification border", 2, 255, "void", 0, False, True, (0, 0, 0)),
+    Label("out of roi", 3, 255, "void", 0, False, True, (0, 0, 0)),
+    Label("static", 4, 255, "void", 0, False, True, (0, 0, 0)),
+    Label("dynamic", 5, 255, "void", 0, False, True, (111, 74, 0)),
+    Label("ground", 6, 255, "void", 0, False, True, (81, 0, 81)),
+    Label("road", 7, 0, "flat", 1, False, False, (128, 64, 128)),
+    Label("sidewalk", 8, 1, "flat", 1, False, False, (244, 35, 232)),
+    Label("parking", 9, 255, "flat", 1, False, True, (250, 170, 160)),
+    Label("rail track", 10, 255, "flat", 1, False, True, (230, 150, 140)),
+    Label("building", 11, 2, "construction", 2, False, False, (70, 70, 70)),
+    Label("wall", 12, 3, "construction", 2, False, False, (102, 102, 156)),
+    Label("fence", 13, 4, "construction", 2, False, False, (190, 153, 153)),
+    Label("guard rail", 14, 255, "construction", 2, False, True, (180, 165, 180)),
+    Label("bridge", 15, 255, "construction", 2, False, True, (150, 100, 100)),
+    Label("tunnel", 16, 255, "construction", 2, False, True, (150, 120, 90)),
+    Label("pole", 17, 5, "object", 3, False, False, (153, 153, 153)),
+    Label("polegroup", 18, 255, "object", 3, False, True, (153, 153, 153)),
+    Label("traffic light", 19, 6, "object", 3, False, False, (250, 170, 30)),
+    Label("traffic sign", 20, 7, "object", 3, False, False, (220, 220, 0)),
+    Label("vegetation", 21, 8, "nature", 4, False, False, (107, 142, 35)),
+    Label("terrain", 22, 9, "nature", 4, False, False, (152, 251, 152)),
+    Label("sky", 23, 10, "sky", 5, False, False, (70, 130, 180)),
+    Label("person", 24, 11, "human", 6, True, False, (220, 20, 60)),
+    Label("rider", 25, 12, "human", 6, True, False, (255, 0, 0)),
+    Label("car", 26, 13, "vehicle", 7, True, False, (0, 0, 142)),
+    Label("truck", 27, 14, "vehicle", 7, True, False, (0, 0, 70)),
+    Label("bus", 28, 15, "vehicle", 7, True, False, (0, 60, 100)),
+    Label("caravan", 29, 255, "vehicle", 7, True, True, (0, 0, 90)),
+    Label("trailer", 30, 255, "vehicle", 7, True, True, (0, 0, 110)),
+    Label("train", 31, 16, "vehicle", 7, True, False, (0, 80, 100)),
+    Label("motorcycle", 32, 17, "vehicle", 7, True, False, (0, 0, 230)),
+    Label("bicycle", 33, 18, "vehicle", 7, True, False, (119, 11, 32)),
+    Label("license plate", -1, -1, "vehicle", 7, False, True, (0, 0, 142)),
+)
+
+# Use only the labels not ignore in the eval split (19 classes)
+used_labels = tuple(label for label in labels if not label.ignoreInEval)
+
+SplitType = Literal["train", "val"]
+
+
+def process_single_file(
+    cityscapes_images_path: Path,
+    cls: Label,
+    cls_annotations_dump_path: Path,
+    images_dump_path: Path,
+    mask_file: Path,
+    split: SplitType,
+):
+    with Image.open(mask_file) as im:
+        mask = np.asarray(im)
+
+    if cls.id not in mask:
+        return False
+
+    mask = (mask == cls.id).astype(np.uint8) * 255
+    mask = Image.fromarray(mask, mode="L")
+
+    city_name = mask_file.parent.name
+    mask_new_path = cls_annotations_dump_path / mask_file.name.replace(
+        "_gtFine_labelIds", ""
+    )
+    mask.save(mask_new_path, optimize=True)
+
+    image_old_path = (
+        cityscapes_images_path
+        / city_name
+        / f"{mask_file.stem.replace('gtFine_labelIds', 'leftImg8bit')}.png"
+    )
+    image_new_path = (
+        images_dump_path / f"{image_old_path.stem.replace('_leftImg8bit', '')}.png"
+    )
+
+    # If the image file already exists skip the copy part below
+    if not image_new_path.exists():
+        shutil.copy(image_old_path, image_new_path)
+
+    return True
+
+
 def process_common_split(
     dump_path: Path,
     cityscapes_path: Path,
-    used_labels: Sequence[Label],
-    split: Literal["train", "val"],
+    max_workers: int | None,
+    split: SplitType,
 ):
     split_long = "validation" if split == "val" else "training"
 
@@ -64,52 +149,65 @@ def process_common_split(
     annotations_dump_path = dump_path / "annotations" / split_long
     annotations_dump_path.mkdir(parents=True, exist_ok=True)
 
-    mask_files = tuple(cityscapes_path.glob(f"gtFine/{split}/**/*gtFine_labelIds.png"))
+    cityscapes_images_path = cityscapes_path / "leftImg8bit" / split
+    if not cityscapes_images_path.is_dir():
+        raise FileNotFoundError(f"{cityscapes_images_path} is not a directory")
 
-    with tqdm(used_labels) as classes:
-        for cls in classes:
+    cityscapes_annotations_path = cityscapes_path / "gtFine" / split
+    mask_files = tuple(cityscapes_annotations_path.rglob("*gtFine_labelIds.png"))
+
+    if not mask_files:
+        raise FileNotFoundError(f"No masks found in {cityscapes_annotations_path}")
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+        future_to_desc: dict[
+            concurrent.futures.Future[bool], tuple[Label, Path, str]
+        ] = {}
+
+        for cls in used_labels:
             cls_annotations_dump_path = annotations_dump_path / cls.name
             cls_annotations_dump_path.mkdir(exist_ok=True)
 
             for mask_idx, mask_file in enumerate(mask_files, 1):
-                classes.set_description(
+                f = executor.submit(
+                    process_single_file,
+                    cityscapes_images_path,
+                    cls,
+                    cls_annotations_dump_path,
+                    images_dump_path,
+                    mask_file,
+                    split,
+                )
+
+                desc = (
                     f"For {split_long} -- class: '{cls.name}', "
                     f"scanned {mask_idx}/{len(mask_files)}"
                 )
+                future_to_desc[f] = (cls, mask_file, desc)
 
-                with Image.open(mask_file) as im:
-                    mask = np.asarray(im)
-
-                if cls.id not in mask:
-                    continue
-
-                mask = (mask == cls.id).astype(np.uint8) * 255
-                mask = Image.fromarray(mask, mode="L")
-
-                city_name = mask_file.parent.name
-                mask_new_path = cls_annotations_dump_path / mask_file.name.replace(
-                    "_gtFine_labelIds", ""
-                )
-                mask.save(mask_new_path, optimize=True)
-
-                image_old_path = (
-                    cityscapes_path
-                    / "leftImg8bit"
-                    / split
-                    / city_name
-                    / f"{mask_file.stem.replace('gtFine_labelIds', 'leftImg8bit')}.png"
-                )
-                image_new_path = (
-                    images_dump_path
-                    / f"{image_old_path.stem.replace('_leftImg8bit', '')}.png"
-                )
-
-                # If the image file already exists skip the copy part below
-                if not image_new_path.exists():
-                    shutil.copy(image_old_path, image_new_path)
+        with tqdm(
+            concurrent.futures.as_completed(future_to_desc),
+            total=len(future_to_desc),
+        ) as pbar:
+            for future in pbar:
+                cls, mask_file, desc = future_to_desc[future]
+                try:
+                    ok = future.result()
+                except Exception as exc:
+                    print(
+                        f"Class name: {cls}, Mask file: {mask_file} generated an exception: {exc}"
+                    )
+                else:
+                    if ok:
+                        pbar.set_description(desc)
 
 
-def main(cityscapes_path: Path, dump_path: Path):
+def main(
+    cityscapes_path: Path,
+    dump_path: Path,
+    split: SplitType | None,
+    max_workers: int | None,
+):
     """Prepare the cityscapes dataset compatible for few shot training.
         <dump_path>
         |---images
@@ -142,56 +240,13 @@ def main(cityscapes_path: Path, dump_path: Path):
     # Make sure to provide your results using the original IDs and not the training IDs.
     # Note that many IDs are ignored in evaluation and thus you never need to predict these!
 
-    labels = (
-        #       name                     id    trainId   category            catId     hasInstances   ignoreInEval   color
-        Label("unlabeled", 0, 255, "void", 0, False, True, (0, 0, 0)),
-        Label("ego vehicle", 1, 255, "void", 0, False, True, (0, 0, 0)),
-        Label("rectification border", 2, 255, "void", 0, False, True, (0, 0, 0)),
-        Label("out of roi", 3, 255, "void", 0, False, True, (0, 0, 0)),
-        Label("static", 4, 255, "void", 0, False, True, (0, 0, 0)),
-        Label("dynamic", 5, 255, "void", 0, False, True, (111, 74, 0)),
-        Label("ground", 6, 255, "void", 0, False, True, (81, 0, 81)),
-        Label("road", 7, 0, "flat", 1, False, False, (128, 64, 128)),
-        Label("sidewalk", 8, 1, "flat", 1, False, False, (244, 35, 232)),
-        Label("parking", 9, 255, "flat", 1, False, True, (250, 170, 160)),
-        Label("rail track", 10, 255, "flat", 1, False, True, (230, 150, 140)),
-        Label("building", 11, 2, "construction", 2, False, False, (70, 70, 70)),
-        Label("wall", 12, 3, "construction", 2, False, False, (102, 102, 156)),
-        Label("fence", 13, 4, "construction", 2, False, False, (190, 153, 153)),
-        Label("guard rail", 14, 255, "construction", 2, False, True, (180, 165, 180)),
-        Label("bridge", 15, 255, "construction", 2, False, True, (150, 100, 100)),
-        Label("tunnel", 16, 255, "construction", 2, False, True, (150, 120, 90)),
-        Label("pole", 17, 5, "object", 3, False, False, (153, 153, 153)),
-        Label("polegroup", 18, 255, "object", 3, False, True, (153, 153, 153)),
-        Label("traffic light", 19, 6, "object", 3, False, False, (250, 170, 30)),
-        Label("traffic sign", 20, 7, "object", 3, False, False, (220, 220, 0)),
-        Label("vegetation", 21, 8, "nature", 4, False, False, (107, 142, 35)),
-        Label("terrain", 22, 9, "nature", 4, False, False, (152, 251, 152)),
-        Label("sky", 23, 10, "sky", 5, False, False, (70, 130, 180)),
-        Label("person", 24, 11, "human", 6, True, False, (220, 20, 60)),
-        Label("rider", 25, 12, "human", 6, True, False, (255, 0, 0)),
-        Label("car", 26, 13, "vehicle", 7, True, False, (0, 0, 142)),
-        Label("truck", 27, 14, "vehicle", 7, True, False, (0, 0, 70)),
-        Label("bus", 28, 15, "vehicle", 7, True, False, (0, 60, 100)),
-        Label("caravan", 29, 255, "vehicle", 7, True, True, (0, 0, 90)),
-        Label("trailer", 30, 255, "vehicle", 7, True, True, (0, 0, 110)),
-        Label("train", 31, 16, "vehicle", 7, True, False, (0, 80, 100)),
-        Label("motorcycle", 32, 17, "vehicle", 7, True, False, (0, 0, 230)),
-        Label("bicycle", 33, 18, "vehicle", 7, True, False, (119, 11, 32)),
-        Label("license plate", -1, -1, "vehicle", 7, False, True, (0, 0, 142)),
-    )
+    common_args = (dump_path, cityscapes_path, max_workers)
 
-    # Use only the labels not ignore in the eval split (19 classes)
-    used_labels = tuple(label for label in labels if not label.ignoreInEval)
-
-    common_args = (dump_path, cityscapes_path, used_labels)
-
-    # Prepare train dataset
-
-    process_common_split(*common_args, split="train")
-
-    # Prepare validation dataset
-    process_common_split(*common_args, split="val")
+    if split is not None:
+        process_common_split(*common_args, split=split)
+    else:
+        for s in typing.get_args(SplitType):
+            process_common_split(*common_args, split=s)
 
 
 if __name__ == "__main__":
@@ -206,16 +261,29 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--cityscapes_path",
+        "--cityscapes-path",
         type=Path,
         required=True,
         help="The path of Cityscapes dataset",
     )
     parser.add_argument(
-        "--dump_path",
+        "--dump-path",
         type=Path,
         help="The path to save new dataset",
         required=True,
+    )
+    parser.add_argument(
+        "--split",
+        type=str,
+        help="The split of dataset to be processed",
+        default=None,
+        choices=typing.get_args(SplitType),
+    )
+    parser.add_argument(
+        "--max-workers",
+        type=int,
+        default=None,
+        help="The number of workers for multiprocessing",
     )
 
     args = parser.parse_args()
